a1c49149f0ac4706d6ff5a265b9ae24aabb1610a
Hoangdu224 <duhoang224@gmail.com>
2023-05-10 19:54:37 +0700

    test

--- angular-cinema-management/src/app/app-routing.module.ts
+++ angular-cinema-management/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminStatisticalMovieComponent} from "./component/admin-statistical/admin-statistical-movie/admin-statistical-movie.component";
-import {AdminStatisticalCustomerComponent} from "./component/admin-statistical/admin-statistical-customer/admin-statistical-customer.component";
-
 
 const routes: Routes = [
-  {path: '', component: AdminStatisticalMovieComponent},
-  {path: 'statistic/movie', component: AdminStatisticalMovieComponent},
-  {path: 'statistic/member', component: AdminStatisticalCustomerComponent},
+  {path: "" , component: AdminStatisticalMovieComponent}
 ];
 
 @NgModule({
--- angular-cinema-management/src/app/component/admin-statistical/admin-statistical-routing.module.ts
+++ angular-cinema-management/src/app/component/admin-statistical/admin-statistical-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AdminStatisticalMovieComponent} from "./admin-statistical-movie/admin-statistical-movie.component";
+import {AdminStatisticalCustomerComponent} from "./admin-statistical-customer/admin-statistical-customer.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'statistic/statistic-movie', component: AdminStatisticalMovieComponent},
+  {path: 'statistic/statistic-customer', component: AdminStatisticalCustomerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

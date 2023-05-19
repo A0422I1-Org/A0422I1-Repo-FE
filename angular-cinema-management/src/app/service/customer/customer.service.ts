6defb2a93afd6c346eed3f500ffafce9c8f13183
trandinhduynghia <trandinhduynghia@gmail.com>
2023-05-19 19:53:46 +0700

    update

--- angular-cinema-management/src/app/service/customer/customer.service.ts
+++ angular-cinema-management/src/app/service/customer/customer.service.ts
@@ -61,16 +61,10 @@ export class CustomerService {
   }
 
   /*
-  Nghia TĐD
-   */
-  getCustomerByUsername(usermame): Observable<Customer>{
-    return this.httpClient.get<Customer>(this.API_URL+'/'+usermame);
-  }
-  /*
     Nghia TĐD
      */
-  updateCustomerUser(id, customer): Observable<any>{
-    return this.httpClient.put('http://localhost:8080/api/user/edit/'+id, customer);
+  updateCustomerUser(customer): Observable<any>{
+    return this.httpClient.put('http://localhost:8080/api/user/edit', customer);
   }
 
 }
